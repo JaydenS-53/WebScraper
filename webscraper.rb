144f5c4bca5048cbf719f5e54e6ebfca83f4b4b0
Jayden Sleeuwenhoek <sleeuwenhj@coventry.ac.uk>
2023-09-20 18:13:39 +0100

    made the GUI look better and added code to search for email addresses, phone numbers and social media accounts

--- webscraper.rb
+++ webscraper.rb
@@ -1,6 +1,7 @@
 require 'net/http'
 require 'nokogiri'
 require 'tk'
+require 'set'
 
 # Define the function to perform scraping
 def scrape_website
@@ -20,38 +21,106 @@ def scrape_website
     h5 = doc.css('h5').map { |h5| h5.text }
  
     # Update the output area with headers
-    $output.value = "H1 Headers:\n#{h1.join("\n")}\n\n"
-    $output.value += "H2 Headers:\n#{h2.join("\n")}\n\n"
-    $output.value += "H3 Headers:\n#{h3.join("\n")}\n\n"
-    $output.value += "H4 Headers:\n#{h4.join("\n")}\n\n"
-    $output.value += "H5 Headers:\n#{h5.join("\n")}"
+    [h1, h2, h3, h4, h5].each_with_index do |headers, index|
+      if !headers.empty?
+        level = index + 1
+        $output.value += "H#{level} Headers:\n\n#{headers.join("\n")}\n\n"
+        $output.insert('end', "-------------------------------------------------------------------------------------------------------\n\n")
+      end
+    end
 
-    # add links to output
+    # extract email addresses
+    email_regex = /\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b/
+    email_addresses = html_content.scan(email_regex).uniq
+    # add emails to output
+    if !email_addresses.empty?
+      $output.value += "Email Addresses:\n\n#{email_addresses.join("\n")}\n\n"
+      $output.insert('end', "-------------------------------------------------------------------------------------------------------\n\n")
+    end
+
+    unique_phone_numbers = Set.new
+
+    # Extract phone numbers using regular expression
+    phone_number_regex = /\b(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})\b/
+    phone_numbers = html_content.scan(phone_number_regex).map { |match| match.compact.join('-') }
+    
+    # Add unique phone numbers to the set
+    phone_numbers.each do |phone|
+      unique_phone_numbers.add(phone)
+    end
+
+    # Add phone numbers to output
+    if !unique_phone_numbers.empty?
+      $output.value += "Phone Numbers:\n\n#{unique_phone_numbers.to_a.join("\n")}\n\n"
+      $output.insert('end', "-------------------------------------------------------------------------------------------------------\n\n")
+    end
+
+    # extract links
     links = doc.css('a').map { |link| link['href'] }
-    $output.value = links.join("\n")
+    # add links to output
+    if !links.empty?
+      $output.value += "Links:\n\n#{links.join("\n")}\n\n"
+      $output.insert('end', "-------------------------------------------------------------------------------------------------------\n\n")
+    end
   else
     $output.value = "Error: #{response.code} #{response.message}"
   end
+
+  social_media_platforms = {
+    'Facebook' => 'facebook.com',
+    'Twitter' => 'twitter.com',
+    'Instagram' => 'instagram.com',
+    'LinkedIn' => 'linkedin.com',
+    'Github' => 'github.com'
+    # Add more platforms as needed
+  }
+
+  social_media_accounts = {}
+
+  social_media_platforms.each do |platform, domain|
+    links = doc.css("a[href*='#{domain}']").map { |link| link['href'] }
+    social_media_accounts[platform] = links unless links.empty?
+  end
+
+  # Update the output area with social media accounts
+  social_media_accounts.each do |platform, accounts|
+    $output.value += "#{platform} Accounts:\n\n#{accounts.join("\n")}\n\n"
+    $output.insert('end', "-------------------------------------------------------------------------------------------------------\n\n")
+  end
+
+
 end
 
 # Create the main application window
 root = TkRoot.new { title "Website Scraper" }
+root.configure(bg: 'black')
 
 # Create an input field
-$entry = TkEntry.new(root) { pack }
+$entry = TkEntry.new(root) { 
+  width 50
+  background 'white'
+  font 'Helvetica 12'
+  pack(pady: 10)
+}
 
 # Create a button to trigger scraping
 TkButton.new(root) {
   text "Scrape Website"
+  background 'white'
+  foreground 'black'
+  font 'Helvetica 12 bold'
   command proc { scrape_website }
-  pack
+  pady 10
+  pack(pady: 10)
 }
 
-# Create an output area to display links
+# Create an output area to display data
 $output = TkText.new(root) {
   width 150
-  height 150
+  height 40
   wrap 'word'
+  background 'black'
+  foreground 'white'
   pack
 }
 
